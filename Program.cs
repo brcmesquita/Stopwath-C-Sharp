e0f214cca3761e84d66b1f80ed6ed9bcd60b67ea
Raphael Cabral <brcmesquita@gmail.com>
2020-11-30 20:08:18 -0300

    last commit

--- Program.cs
+++ Program.cs
@@ -19,7 +19,35 @@ namespace Stopwatch
       System.Console.WriteLine("Quanto tempo deseja contar?");
 
       string data = Console.ReadLine().ToLower();
-      Console.WriteLine(data);
+      char type = char.Parse(data.Substring(data.Length - 1, 1));
+      int time = int.Parse(data.Substring(0, data.Length - 1));
+      int multiplier = 1;
+
+      if (type == 'm')
+        multiplier = 60;
+
+      if (time == 0)
+        System.Environment.Exit(0);
+
+      PreStart(time * multiplier);
+
+    }
+
+    static void PreStart(int time)
+    {
+      Console.Clear();
+      System.Console.WriteLine("Ready...");
+      Thread.Sleep(1000);
+
+      Console.Clear();
+      System.Console.WriteLine("Set...");
+      Thread.Sleep(1000);
+
+      Console.Clear();
+      System.Console.WriteLine("Go!");
+      Thread.Sleep(1000);
+
+      Start(time);
     }
 
     static void Start(int time)
@@ -32,12 +60,16 @@ namespace Stopwatch
         Console.Clear();
         currentTime++;
         Console.WriteLine(currentTime);
-        Thread.Sleep(1000);
+        // Thread.Sleep(500);
+        // System.Console.Write(" -> ");
+        // Thread.Sleep(500);
+
       }
 
       Console.Clear();
       System.Console.WriteLine("Stopwatch finalizado.");
       Thread.Sleep(2500);
+      Menu();
     }
   }
 }
